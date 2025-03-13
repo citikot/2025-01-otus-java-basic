8dbf35f8119ab49c539a492945e85975123d436c
citikot <citikot@dr.com>
2025-03-13 09:03:56 +0200

    created homework7 module

---
+++ homework7/src/ru/otus/homework7/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.homework7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 7.");
+        System.out.println("==================");
+    }
+}
