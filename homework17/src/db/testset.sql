9ff1ae5bcbf3bf2a56fb372a0666c97369383cb4
Oleksandr Pishvanov <oleksandrp@paynexpay.com>
2025-05-14 17:33:56 +0300

    added code review changes

--- homework17/src/db/testset.sql
+++ homework17/src/db/testset.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `otus_db`.`testset` (
   `id_testset` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(200) NULL,
+  `name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`id_testset`),
   UNIQUE INDEX `id_testset_UNIQUE` (`id_testset` ASC) VISIBLE);
